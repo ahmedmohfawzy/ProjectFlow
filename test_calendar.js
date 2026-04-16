6b68db195ed9e9c92bb8ffb68f555b222a75d215
Ahmed Mohammed Fawzy <Ahmed.m.fawzy@hotmail.com>
2026-04-16 22:33:33 +0200

    Clean: remove docs, database, temp files from repo (local only)

--- test_calendar.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const js = fs.readFileSync('js/calendar.js', 'utf8');
-eval(js);
-try {
-    let start = new Date('2024-01-01');
-    let end = new Date('2024-01-10');
-    console.log("getWorkingDays:", WorkCalendar.getWorkingDays(start, end));
-    console.log("addWorkingDays:", WorkCalendar.addWorkingDays(start, 5));
-} catch(e) { console.error("ERR:", e); }
